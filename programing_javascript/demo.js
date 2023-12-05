94e86d9582ea4572ae7393ee0ac88082195c906d
Borgerod <borgerod@hotmail.com>
2023-12-05 15:32:53 +0100

    end of day push 2

---
+++ programing_javascript/demo.js
@@ -0,0 +1,28 @@
+// (0) _____ BASE FUNCTIONS __________________________________  
+
+// Print function
+console.log("Hello World")
+
+
+// (1) _____ VARIABLES __________________________________  
+// NOTE: Javascript does not use types ( num, int, float, string) notation for variables
+//       which is why Typescript was made. 
+
+
+// Variables that can be changed / is fluid [DEPRICATED, DO NOT USE THIS]
+var outdatedVariable = "foobar"
+
+// save variables for later use / is fluid
+let variableName = "fooabr";
+
+// variables which cannon be changed / is constant 
+const constantName = 2000;
+
+// Changing variables
+variableName = "jibb jabb"
+
+// Equations
+let tallA = 20
+let tallB = 30
+let sum = tallA + tallB
+
