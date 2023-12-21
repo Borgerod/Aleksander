93ea2cc288c579981fe574ca27a3e87d8e599041
Borgerod <borgerod@hotmail.com>
2023-12-21 13:05:03 +0100

    added to and simplified darkmode button

--- business_website/javascript/darkmode_button.js
+++ business_website/javascript/darkmode_button.js
@@ -1,18 +1,18 @@
-let lightmodePopUp = document.querySelector("bodylightmode");
-lightmodePopUp.style.display="none";
-
 function lightmodeButton() {
-        let snd = new Audio("/media/audio/overthrone.mp3");
-        snd.play();
-        
-        // make lightmode popup vissible
-        lightmodePopUp.style.display = "block";
+      let audio = new Audio("/media/audio/overthrone.mp3");
+      let lightmodePopUp = document.querySelector("bodylightmode");
+      
+      
 
-        // trigger transition after 5.5s
-        setTimeout(function() {lightmodePopUp.classList.toggle("dark-light-mode-transition")}, 5500);
-        
-        // pause sound and close popup after 9s
-        setTimeout(function() {snd.pause()}, 9000);
-        setTimeout(function() {lightmodePopUp.style.display = "none";}, 9000);
+      audio.onplay = function() {
+        // Show the image when the audio starts playing
+        lightmodePopUp.style.visibility = "visible";
+      };
+
+      audio.play();
+      lightmodePopUp.requestFullscreen();
+
+      // pause sound and close popup after 9s
+      setTimeout(function() {audio.pause()}, 9000);
+      setTimeout(function() {lightmodePopUp.style.visibility = "hidden";}, 9000);
 }
-
